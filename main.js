af87574303157acdb161bb6c5528ecec7061c715
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-03-18 01:24:47 -0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const botoes=document.querySelectorAll(".botao");
+for (let i=0; i<botoes.length; i++){
+    botoes[i].onclick=function(){
+        for (let j=0; j<botoes.lenghth;j++){
+            botoes[j].classList.remove("ative");
+        }
+        
+        botoes[i].classList.add("ativo");
+    }
+}
