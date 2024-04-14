de862c4633982699b5307e05329cf55af1b366f5
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-04-14 17:36:54 -0300

    ok

--- main.js
+++ main.js
@@ -16,4 +16,5 @@ for(let i=0; i<botoes.length;i++){
 
 const contadores= document.querySelectorAll(".contador");
 const tempoObjetivo1= new Date("2024-10-05T00:00:00");
-contadores[0].textContent=tempoObjetivo1;
+let tempoAtual=newDate ();
+contadores[0].textContent=tempoObjetivo1- tempoAtual;
